d65c37cf8da8f7970536fcdb9c0c098f96343c4a
Jai Suriya <85334558+jairoyy@users.noreply.github.com>
2024-11-08 17:25:10 +0530

    Add files via upload

--- myapp/settings.py
+++ myapp/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#!==@c-lt!ii--w3j%5c@=9bv9762b@n9qx#7mwrxz7nq+66^a'
+SECRET_KEY = 'django-insecure-md^m_ji+976z&zgzsx4zdw=3s+^g%x1m@#pa+wqpfxrp!%a!xm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- myblog/views.py
+++ myblog/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, This is my first blog")
+
+def detail(request ,post_id):
+    return HttpResponse(f"You are viewing blog detail page and ID is {post_id} ")
